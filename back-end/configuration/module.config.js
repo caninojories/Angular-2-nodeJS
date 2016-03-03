a6df535a4381f24fd926eecc3367ff667af1a39b
Jo-Ries Canino <canino_jories@hotmail.com>
2016-03-03 10:22:18 +0800

    adding back-end

---
+++ back-end/configuration/module.config.js
@@ -0,0 +1,39 @@
+(function() {
+  'use strict';
+
+  var service = '../services/';
+
+  module.exports = {
+    config            : require('./settings.config'),
+    mongoDB           : require('../configuration/mongodb'),
+    use_app           : require('./use_app.config'),
+    use_api           : require('./use_api.config'),
+
+    args              : require('yargs').argv,
+    bodyParser        : require('body-parser'),
+    chalk             : require('chalk'),
+    cluster           : require('cluster'),
+    compression       : require('compression'),
+    express           : require('express'),
+    favicon           : require('serve-favicon'),
+    morgan            : require('morgan'),
+    methodOverride    : require('method-override'),
+    mongoose          : require('mongoose'),
+    numCPUs           : require('os').cpus().length,
+    path              : require('path'),
+    serveStatic       : require('serve-static'),
+    url               : require('url'),
+
+    port              : process.env.PORT || 3000,
+    environment       : process.env.NODE_ENV || 'development',
+    // faviconPath       : rootPath + 'front-end/resources/favicon.ico',
+
+    /*Services*/
+    xPoweredBy        : require(service + 'xPoweredBy'),
+    clusterService    : require(service + './cluster'),
+    /* Models */
+    User              : require('../model/User'),
+    /* Classes */
+    USER              : require('../classes/user.js'),
+  };
+}());
---
+++ back-end/model/User.js
@@ -0,0 +1,21 @@
+(function() {
+  'use strict';
+
+  var mongoose    = require('mongoose'),
+      timestamps  = require('mongoose-timestamp');
+
+  var UserSchema = new mongoose.Schema({
+    email : {
+      type    : String,
+      required: true,
+      unique  : true
+    },
+    name  : String,
+    gender: String,
+    color : String
+  });
+
+  UserSchema.plugin(timestamps);
+
+  module.exports = mongoose.model('User', UserSchema);
+}());
