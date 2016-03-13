da94a67265c60042a7d384ca482c6e77e7cd6679
Jo-Ries Canino <canino_jories@hotmail.com>
2016-03-14 03:33:48 +0800

    sending hostname to index.html

--- back-end/routes/index.js
+++ back-end/routes/index.js
@@ -2,14 +2,17 @@
   'use strict';
 
   var router = io.express.Router();
-  
+
   router.use(function timeLog(req, res, next) {
     console.log('Time: ', Date.now());
     next();
   });
 
   router.get('/', io.xPoweredBy, function(req, res) {
-    res.render('index.html');
+    console.log(req.headers);
+    res.render('index.html', {
+      ip : 'http://' + req.headers.host.replace(":3001", '')
+    });
   });
 
   module.exports = router;
